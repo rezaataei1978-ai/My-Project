1cd0110ef3c1dabbda417188659d422151cc033f
rezaataei1978-ai <rezaataei1978@gmail.com>
2025-11-21 09:21:36 +0330

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,10 @@
+#if statement
+age=14
+if age>18:
+   print("you are allowed")
+elif age == 18:
+   print("you are 18")
+elif age == 16:
+   print("you are 16")
+else:
+   print("you are not allowed")
